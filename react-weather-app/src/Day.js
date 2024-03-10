cdb4f5b95e3e9db925f4277d56a13074a5b54f6c
NickiCodesNow <luftkuchen15@gmail.com>
2024-03-10 13:34:29 +0100

    finish import of sandbox static project -Fix link images in JS files and import of bootstrap for styling

--- react-weather-app/src/Day.js
+++ react-weather-app/src/Day.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Day(props) {
   return (
     <div className="col-2">
-      <img className="forecast-img" src="../public/images/Cloud.png" alt="" />
+      <img className="forecast-img" src="../images/Cloud.png" alt="" />
       <br />
       <span className="forecast-temp">{props.temp}°C</span>
       <br />
