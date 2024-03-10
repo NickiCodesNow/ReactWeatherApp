90ce771cb3965b4fdb569eac48111b879b045b93
NickiCodesNow <luftkuchen15@gmail.com>
2024-03-10 13:35:57 +0100

    Fix Link to GitHubProject

--- react-weather-app/src/Footer.js
+++ react-weather-app/src/Footer.js
@@ -5,7 +5,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <a href="https://github.com/NickiCodesNow/ReactWeatherApps" target="_blank">
+      <a href="https://github.com/NickiCodesNow/ReactWeatherApp" target="_blank">
         Open-source code
       </a>{" "}
       by NickiCodesNow
